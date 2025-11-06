214d42339d9a2061fe1c966e27fe6e418c10b590
Animainmilol <animainmilol@gmail.com>
2025-11-06 20:03:55 +0500

    Let's use GTK4 and use GTK4 example, not layer shell though

--- main.go
+++ main.go
@@ -1,54 +1,25 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/diamondburned/gotk4-layer-shell/pkg/gtklayershell"
-	"github.com/diamondburned/gotk4/pkg/gtk/v3"
-	"github.com/diamondburned/gotk4/pkg/pango"
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
 )
 
 func main() {
-	app := gtk.NewApplication("com.github.animainmi.VoBriGo", 0)
-	app.Connect("activate", activate)
+	app := gtk.NewApplication("com.github.animainmi.VoBriGo", gio.ApplicationFlagsNone)
+	app.ConnectActivate(func() { activate(app) })
 
 	if code := app.Run(os.Args); code > 0 {
 		os.Exit(code)
 	}
 }
 
-var attrs = []*pango.Attribute{
-	pango.NewAttrScale(10),
-	pango.NewAttrWeight(pango.WeightBold),
-}
-
 func activate(app *gtk.Application) {
-	if !gtklayershell.IsSupported() {
-		log.Fatalln("gtk-layer-shell not supported")
-	}
-
-	labelAttrs := pango.NewAttrList()
-	for _, attr := range attrs {
-		labelAttrs.Insert(attr)
-	}
-
-	label := gtk.NewLabel("This window floats in the middle.")
-	label.SetAttributes(labelAttrs)
-	label.Show()
-
-	appwin := gtk.NewApplicationWindow(app)
-	appwin.Add(label)
-	appwin.SetTitle("Layer Shell Example")
-
-	window := &appwin.Window
-
-	gtklayershell.InitForWindow(window)
-	gtklayershell.SetLayer(window, gtklayershell.LayerShellLayerTop)
-
-	for edge := gtklayershell.Edge(0); edge < gtklayershell.LayerShellEdgeEntryNumber; edge++ {
-		gtklayershell.SetAnchor(window, edge, false)
-	}
-
-	appwin.Show()
+	window := gtk.NewApplicationWindow(app)
+	window.SetTitle("gotk4 Example")
+	window.SetChild(gtk.NewLabel("Hello from Go!"))
+	window.SetDefaultSize(400, 300)
+	window.Show()
 }
